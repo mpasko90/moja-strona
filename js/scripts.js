8a0dac07e1b66d956345d8fc05e575ce5d5a525d
mpasko90 <91828918+mpasko90@users.noreply.github.com>
2023-02-02 19:04:06 +0100

    nowa wersja strony przed dodaniem url content 70%

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,6 @@
-jQuery(document).ready(function ($) {
-    // Scroll to the desired section on click
-    // Make sure to add the `data-scroll` attribute to your `<a>`          tag.
-    // Example: 
-    // `<a data-scroll href="#my-section">My Section</a>` will scroll to an element with the id of 'my-section'.
-    function scrollToSection(event) {
-        event.preventDefault();
-        var $section = $($(this).attr('href'));
-        $('html, body').animate({
-            scrollTop: $section.offset().top
-        }, 500);
-    }
-    $('[data-scroll]').on('click', scrollToSection);
-}(jQuery));
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
 
-//linke tıklandığında active class ekle
-//menüyü kapat
-//change active status
-$(".nav li a").click(function () {
-    $(this).parent().addClass('active').siblings().removeClass('active');
-    $("#navbar").removeClass("in");
-});
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
